20341ef0c2cecf0e8e9c152792a1d7d27d517aed
cyk1337 <s1718204@sms.ed.ac.uk>
2018-07-03 21:00:44 +0100

    crf self-training result

--- demo/static/js/demo.js
+++ demo/static/js/demo.js
@@ -4,10 +4,10 @@ $(function () {
             $(this).removeClass('btn-primary').addClass('btn-danger');
             $(this).attr('value', 'Searching...')
         }
-    })
+    });
 
     $("#model").change(function () {
-        var value = $("#model option:selected").val();
+        var value = $("#model option[]:selected").val();
         if (value == 'bt') {
             // $(this).parent().nextAll('.form-group').fadeOut(300, function(){ $(this).remove();});
             $(this).parent().nextAll('.form-group').collapse('hide');
