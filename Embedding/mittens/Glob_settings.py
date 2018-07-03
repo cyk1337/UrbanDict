6b5b64d3697abf758339f86b416159f6e95f7920
cyk1337 <s1718204@sms.ed.ac.uk>
2018-07-03 22:50:24 +0100

    crf self-training result

--- Embedding/mittens/Glob_settings.py
+++ Embedding/mittens/Glob_settings.py
@@ -24,6 +24,13 @@
 '''              
 import os
 
-data_dir= 'Volumes/Ed/data'
+data_dir= '/Volumes/Ed/data'
 wiki_dir = os.path.join(data_dir, 'wiki')
 tweet_dir = os.path.join(data_dir, 'tweet')
+
+simpwiki_bz = os.path.join(wiki_dir, 'simplewiki-20180701-pages-articles.xml.bz2')
+simpwiki_file = os.path.join(wiki_dir, 'simplewiki.txt')
+
+enwiki_bz = os.path.join(wiki_dir, 'enwiki-20180701-pages-articles-multistream.xml.bz2')
+enwiki_file = os.path.join(wiki_dir, 'enwiki.txt')
+
---
+++ Embedding/mittens/gen_wiki.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+#-*- encoding: utf-8 
+
+'''
+                      ______   ___  __
+                     / ___\ \ / / |/ /
+                    | |    \ V /| ' / 
+                    | |___  | | | . \ 
+                     \____| |_| |_|\_\
+ ==========================================================================
+@author: Yekun Chai
+
+@license: School of Informatics, Edinburgh
+
+@contact: s1718204@sms.ed.ac.uk
+
+@file: gen_wiki.py
+
+@time: 03/07/2018 21:05 
+
+@desc：       
+               
+'''              
+from __future__ import print_function
+import logging
+
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Glob_settings import *
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+logging.root.setLevel(level=logging.INFO)
+
+from gensim.corpora import WikiCorpus
+
+wiki_bz = simpwiki_bz
+wiki_file = simpwiki_file
+
+wiki = WikiCorpus(wiki_bz, lemmatize=False, dictionary={})
+
+f = open(wiki_file, 'a')
+
+for i, text in enumerate(wiki.get_texts()):
+    f.write(' '.join(text) + '\n')
+    if (i % 10000 == 0):
+        logger.info("Saved " + str(i) + " articles")
