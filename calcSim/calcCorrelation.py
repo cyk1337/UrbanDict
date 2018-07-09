56c059011d9bd9032273d356b6665c5028cc4a09
cyk1337 <s1718204@sms.ed.ac.uk>
2018-07-09 22:40:09 +0100

    tune

--- calcSim/calcCorrelation.py
+++ calcSim/calcCorrelation.py
@@ -30,9 +30,9 @@ c=0
 val_acc = np.array([0.9322, 0.9496, 0.9444])
 
 # min count 20
-var_map1 = np.array([6, 7, 6]) / 305
-var_map2 = np.array([16, 25, 16])/ 305
-var_map3 = np.array([20, 26, 20])/ 305
+var_map1 = np.array([7, 6, 6]) / 305
+var_map2 = np.array([25, 16, 16])/ 305
+var_map3 = np.array([26,20, 20])/ 305
 
 
 c1 = pearsonr(val_acc, var_map1)[0]
@@ -46,9 +46,9 @@ c+=c0
 print("Min count20, vec size50, avg:{:.4f}, c1:{:.4f}, c2:{:.4f}, c3{:.4f}".format(c0,c1,c2,c3))
 
 # min count 5, size 50
-var_map1 = np.array([5, 7, 4]) / 386
-var_map2 = np.array([13, 17, 16])/ 386
-var_map3 = np.array([15, 24, 19])/ 386
+var_map1 = np.array([7, 5, 4]) / 386
+var_map2 = np.array([17, 13, 16])/ 386
+var_map3 = np.array([24, 15, 19])/ 386
 c1 = pearsonr(val_acc, var_map1)[0]
 c2 = pearsonr(val_acc, var_map2)[0]
 c3 = pearsonr(val_acc, var_map3)[0]
@@ -57,9 +57,9 @@ c+=c0
 print("Min count5, vec size50, avg:{:.4f}, c1:{:.4f}, c2:{:.4f}, c3{:.4f}".format(c0,c1,c2,c3))
 
 # min count 5, size 50 remove #,@,link
-var_map1 = np.array([6, 7, 5]) / 386
-var_map2 = np.array([15, 19, 18])/ 386
-var_map3 = np.array([17, 25, 22])/ 386
+var_map1 = np.array([7,6, 5]) / 386
+var_map2 = np.array([19, 15, 18])/ 386
+var_map3 = np.array([25, 17, 22])/ 386
 c1 = pearsonr(val_acc, var_map1)[0]
 c2 = pearsonr(val_acc, var_map2)[0]
 c3 = pearsonr(val_acc, var_map3)[0]
@@ -68,9 +68,9 @@ c+=c0
 print("Min count5, vec size50, rm #@http, avg:{:.4f}, c1:{:.4f}, c2:{:.4f}, c3{:.4f}".format(c0,c1,c2,c3))
 
 # min count 5, size 100
-var_map1 = np.array([6, 9, 2]) / 386
-var_map2 = np.array([11, 19, 16])/ 386
-var_map3 = np.array([15, 26, 23])/ 386
+var_map1 = np.array([9, 6,  2]) / 386
+var_map2 = np.array([19,11, 16])/ 386
+var_map3 = np.array([26, 15, 23])/ 386
 c1 = pearsonr(val_acc, var_map1)[0]
 c2 = pearsonr(val_acc, var_map2)[0]
 c3 = pearsonr(val_acc, var_map3)[0]
@@ -79,9 +79,9 @@ c+=c0
 print("Min count5, vec size100, avg:{:.4f}, c1:{:.4f}, c2:{:.4f}, c3{:.4f}".format(c0,c1,c2,c3))
 
 # min count 5, size 100 remove #,@,link
-var_map1 = np.array([6, 9, 5]) / 386
-var_map2 = np.array([13, 19, 21])/ 386
-var_map3 = np.array([17, 27, 27])/ 386
+var_map1 = np.array([9, 6, 5]) / 386
+var_map2 = np.array([19, 13, 21])/ 386
+var_map3 = np.array([27, 17, 27])/ 386
 c1 = pearsonr(val_acc, var_map1)[0]
 c2 = pearsonr(val_acc, var_map2)[0]
 c3 = pearsonr(val_acc, var_map3)[0]
